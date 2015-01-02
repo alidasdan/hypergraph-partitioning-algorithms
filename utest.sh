57387b17993771f2230732efb0462140f7eb5d76
Ali Dasdan <dasdan@dreamoid.com>
2015-01-01 23:17:11 -0800

    added the second simulated annealing algo

--- utest.sh
+++ utest.sh
@@ -13,6 +13,7 @@
 ./ad_pfm.x ./input/hp1 2 3 1 123456 | grep Final | awk -v p="ad_pfm p1 2 3 1" -v t=1 -f utest.awk
 
 ./ad_sa1.x ./input/hp1 2 123456 | grep Final | awk -v p="ad_sa1 p1 2" -v t=1 -f utest.awk
+./ad_sa2.x ./input/hp1 2 123456 | grep Final | awk -v p="ad_sa2 p1 2" -v t=1 -f utest.awk
 
 # testing each executable using input/hp9
 
@@ -35,6 +36,8 @@
 
 ./ad_sa1.x ./input/hp9 2 123456 | grep Final | awk -v p="ad_sa1 p9 2" -v t=40 -f utest.awk
 ./ad_sa1.x ./input/hp9 3 123456 | grep Final | awk -v p="ad_sa1 p9 3" -v t=56 -f utest.awk
+./ad_sa2.x ./input/hp9 2 123456 | grep Final | awk -v p="ad_sa2 p9 2" -v t=87 -f utest.awk
+./ad_sa2.x ./input/hp9 3 123456 | grep Final | awk -v p="ad_sa2 p9 3" -v t=119 -f utest.awk
 
 # EOF
 
